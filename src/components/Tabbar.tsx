fdf033c6eb5c8373a4de352e35326988bdcbb9bb
devchaen <cogus1221@gmail.com>
2024-03-15 03:47:24 +0900

    [feat] TabBar 기능 수정 : 최초 렌더링 시 특정 경로인 경우, 탭 자동 선택

--- src/components/Tabbar.tsx
+++ src/components/Tabbar.tsx
@@ -1,42 +1,76 @@
 import { Badge, TabBar } from 'antd-mobile';
-import { useCallback, useState } from 'react';
-import Chat from '@/assets/icons/message.svg?react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import HomeIcon from '@/assets/icons/home.svg?react';
+import FeedsIcon from '@/assets/icons/feed.svg?react';
+import NewPostIcon from '@/assets/icons/newPost.svg?react';
+import ChatIcon from '@/assets/icons/message.svg?react';
+import MypageIcon from '@/assets/icons/mypage.svg?react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const tabs = [
   {
     key: 'home',
     title: '홈',
-    icon: <img src="src/assets/icons/home.svg" />,
+    icon: <HomeIcon />,
   },
   {
     key: 'feeds',
     title: '피드',
-    icon: <img src="src/assets/icons/feed.svg" />,
+    icon: <FeedsIcon />,
   },
   {
     key: 'newPost',
     title: '새 게시물',
-    icon: <img src="src/assets/icons/newPost.svg" />,
+    icon: <NewPostIcon />,
   },
   {
     key: 'chat',
     title: '메시지',
-    icon: <Chat />,
+    icon: <ChatIcon />,
     badge: Badge.dot,
   },
   {
     key: 'user',
     title: '마이페이지',
-    icon: <img src="src/assets/icons/mypage.svg" />,
+    icon: <MypageIcon />,
   },
 ];
 
 const Tabbar = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
-  const [activeKey, setActiveKey] = useState('home');
+  const [activeKey, setActiveKey] = useState<string>('');
   const userId = '1';
 
+  /*
+    주소창으로 접속 시 or 새로고침 : pathname으로 tab 자동 선택 (activeKey값 설정)
+  */
+  const syncTabWithPath = useCallback((pathname: string) => {
+    if (pathname === '/posts/create') setActiveKey('newPost');
+    else if (pathname.split('/')[2] === userId) setActiveKey('user');
+    else {
+      switch (pathname.split('/')[1]) {
+        case 'home':
+          setActiveKey('home');
+          break;
+        case 'feeds':
+          setActiveKey('feeds');
+          break;
+        case 'chat':
+          setActiveKey('chat');
+          break;
+      }
+    }
+  }, []);
+
+  // 최초 렌더링 시에만 syncTabWithPath 함수 실행
+  useEffect(() => {
+    syncTabWithPath(pathname);
+  }, []);
+
+  /*
+    탭 Item 선택 시 해당 화면으로 Navigate
+  */
   const navigateRoute = useCallback(
     (key: string) => {
       setActiveKey(key);
@@ -45,26 +79,26 @@ const Tabbar = () => {
       switch (key) {
         case 'home':
           navigate('/home');
-          return;
+          break;
         case 'feeds':
           navigate('/feeds');
-          return;
+          break;
         case 'newPost':
           navigate('/posts/create');
-          return;
+          break;
         case 'chat':
           navigate('/chat');
-          return;
+          break;
         case 'user':
           navigate(`/user/${userId}`); // current user Id로 넣어줘야 함!!!
-          return;
+          break;
       }
     },
     [userId],
   );
 
   return (
-    <TabBar onChange={navigateRoute}>
+    <TabBar onChange={navigateRoute} activeKey={activeKey}>
       {tabs.map((item) => (
         <TabBar.Item
           key={item.key}
