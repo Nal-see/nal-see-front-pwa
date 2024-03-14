a38fcd4fa8066f5725bc6d68d005a6857e6f8387
devchaen <cogus1221@gmail.com>
2024-03-15 05:30:29 +0900

    [feat] 하단 내비게이션 TabBar : svgr cli 설정, 탭이 선택되면 icon 색 변화

--- src/components/Tabbar.tsx
+++ src/components/Tabbar.tsx
@@ -1,46 +1,94 @@
 import { Badge, TabBar } from 'antd-mobile';
 import { useCallback, useEffect, useState } from 'react';
-import HomeIcon from '@/assets/icons/home.svg?react';
-import FeedsIcon from '@/assets/icons/feed.svg?react';
-import NewPostIcon from '@/assets/icons/newPost.svg?react';
-import ChatIcon from '@/assets/icons/message.svg?react';
-import MypageIcon from '@/assets/icons/mypage.svg?react';
 import { useLocation, useNavigate } from 'react-router-dom';
-
-const tabs = [
-  {
-    key: 'home',
-    title: '홈',
-    icon: <HomeIcon />,
-  },
-  {
-    key: 'feeds',
-    title: '피드',
-    icon: <FeedsIcon />,
-  },
-  {
-    key: 'newPost',
-    title: '새 게시물',
-    icon: <NewPostIcon />,
-  },
-  {
-    key: 'chat',
-    title: '메시지',
-    icon: <ChatIcon />,
-    badge: Badge.dot,
-  },
-  {
-    key: 'user',
-    title: '마이페이지',
-    icon: <MypageIcon />,
-  },
-];
+import {
+  FeedIcon,
+  HomeIcon,
+  MessageIcon,
+  MypageIcon,
+  NewPostIcon,
+} from './icons';
 
 const Tabbar = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const [activeKey, setActiveKey] = useState<string>('');
   const userId = '1';
+
+  const tabs = [
+    {
+      key: 'home',
+      title: '홈',
+      icon: (
+        <HomeIcon
+          mainColor={
+            activeKey === 'home' ? 'var(--accent)' : 'var(--secondary)'
+          }
+        />
+      ),
+    },
+    {
+      key: 'feeds',
+      title: '피드',
+      icon: (
+        <FeedIcon
+          mainColor={
+            activeKey === 'feeds' ? 'var(--accent)' : 'var(--secondary)'
+          }
+          subColor={
+            activeKey === 'feeds'
+              ? 'var(--accent-foreground)'
+              : 'var(--secondary-foreground)'
+          }
+        />
+      ),
+    },
+    {
+      key: 'newPost',
+      title: '새 게시물',
+      icon: (
+        <NewPostIcon
+          mainColor={
+            activeKey === 'newPost' ? 'var(--accent)' : 'var(--secondary)'
+          }
+          subColor={
+            activeKey === 'newPost'
+              ? 'var(--accent-foreground)'
+              : 'var(--secondary-foreground)'
+          }
+        />
+      ),
+    },
+    {
+      key: 'chat',
+      title: '메시지',
+      icon: (
+        <MessageIcon
+          mainColor={activeKey === 'chat' ? 'var(--accent)' : 'var(--secondary'}
+          subColor={
+            activeKey === 'chat'
+              ? 'var(--accent-foreground)'
+              : 'var(--secondary-foreground'
+          }
+        />
+      ),
+      badge: Badge.dot,
+    },
+    {
+      key: 'user',
+      title: '마이페이지',
+      icon: (
+        <MypageIcon
+          mainColor={activeKey === 'user' ? 'var(--accent)' : 'var(--secondary'}
+          subColor={
+            activeKey === 'user'
+              ? 'var(--accent-foreground)'
+              : 'var(--secondary-foreground'
+          }
+        />
+      ),
+    },
+  ];
 
   /*
     주소창으로 접속 시 or 새로고침 : pathname으로 tab 자동 선택 (activeKey값 설정)
@@ -63,10 +111,9 @@ const Tabbar = () => {
     }
   }, []);
 
-  // 최초 렌더링 시에만 syncTabWithPath 함수 실행
   useEffect(() => {
     syncTabWithPath(pathname);
-  }, []);
+  }, [pathname]);
 
   /*
     탭 Item 선택 시 해당 화면으로 Navigate
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as FeedIcon } from './Feed';
+export { default as HomeIcon } from './Home';
+export { default as MessageIcon } from './Message';
+export { default as MypageIcon } from './Mypage';
+export { default as NewPostIcon } from './NewPost';
