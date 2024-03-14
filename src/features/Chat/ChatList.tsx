5ccb3172475da1b4d2579793bd6422fd2a1ac784
devchaen <cogus1221@gmail.com>
2024-03-15 05:27:32 +0900

    [UI] 뒤로가기 버튼이 있는 header 컴포넌트, private route 전역 레이아웃 조정

--- src/features/Chat/ChatList.tsx
+++ src/features/Chat/ChatList.tsx
@@ -1,6 +1,9 @@
+import BackBtnHeader from './components/BackBtnHeader';
+
 const ChatListPage = () => {
   return (
     <div>
+      <BackBtnHeader title="메시지" />
       <p>ChatListPage</p>
     </div>
   );
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -5,12 +5,12 @@ import Tabbar from '@/components/Tabbar';
 const PrivateRoute = () => {
   const { isAuthenticated } = useAuthStore();
   return isAuthenticated ? (
-    <>
+    <div className="pt-10">
       <Outlet />
       <div className="fixed bottom-0 w-full border-t px-[27px] py-2 pb-[29px]">
         <Tabbar />
       </div>
-    </>
+    </div>
   ) : (
     <Navigate to="/" />
   );
