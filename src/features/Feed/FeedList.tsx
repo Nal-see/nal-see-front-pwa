97ee425e3e88db2e7c40737ace2e0ea3c46deff3
oyeong011 <oyeong011@gmail.com>
2024-03-15 10:40:48 +0900

    [feat] 리액트 쿼리 기능 생성 및 피드 리스트 페이지 생성

--- src/features/Feed/FeedList.tsx
+++ src/features/Feed/FeedList.tsx
@@ -1,11 +1,51 @@
-import Antdesign from '@/components/Antdesign';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import React from 'react';
+import FeedCard from './components/FeedCard';
+import { useInView } from 'react-intersection-observer';
+import { getFeedList } from './services/feedApi';
+import { Feed } from '@/types/feed';
 
 const FeedListPage = () => {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery<Feed[], string[], string | undefined>({
+      queryKey: ['feedList'],
+      queryFn: async ({
+        pageParam = 10,
+      }: {
+        pageParam?: number | undefined;
+      }) => {
+        console.log('pageParam: ', pageParam);
+        const response = await getFeedList(pageParam);
+        return response;
+      },
+      getNextPageParam: (lastPage) => {
+        const lastFeed = lastPage[lastPage.length - 1];
+        return lastFeed ? lastFeed.id : undefined;
+      },
+      initialPageParam: undefined, // Add this line
+    });
+
+  const [ref, inView] = useInView({
+    threshold: 0,
+  });
+
+  React.useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
   return (
     <div>
-      <p>FeedList page</p>
-      {/* 제거예정 */}
-      <Antdesign />
+      {data?.pages.map((page, pageIndex) => (
+        <React.Fragment key={pageIndex}>
+          {page.map((feed: Feed) => (
+            <FeedCard key={feed.id} feed={feed} />
+          ))}
+        </React.Fragment>
+      ))}
+      {isFetchingNextPage && <div>Loading...</div>}
+      <div ref={ref} />
     </div>
   );
 };
