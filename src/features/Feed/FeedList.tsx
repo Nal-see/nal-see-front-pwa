362d7c8899c3a0603400c9c21308f37cf8c07c7b
oyeong011 <oyeong011@gmail.com>
2024-03-20 10:20:35 +0900

    [test] NS-12 아마존 ec2 테스트용

--- src/features/Feed/FeedList.tsx
+++ src/features/Feed/FeedList.tsx
@@ -1,54 +1,54 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
-import React from 'react';
-import FeedCard from './components/FeedCard';
-import { useInView } from 'react-intersection-observer';
-import { getFeedList } from './services/feedApi';
-import { Feed } from '@/types/feed';
+// import { useInfiniteQuery } from '@tanstack/react-query';
+// import React from 'react';
+// import FeedCard from './components/FeedCard';
+// import { useInView } from 'react-intersection-observer';
+// import { getFeedList } from './services/feedApi';
+// import { Feed } from '@/types/feed';
 
-const FeedList = () => {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery<Feed[], string[], string | undefined>({
-      queryKey: ['feedList'],
-      queryFn: async ({
-        pageParam = 10,
-      }: {
-        pageParam?: number | undefined;
-      }) => {
-        const response = await getFeedList(pageParam);
-        return response;
-      },
-      getNextPageParam: (lastPage) => {
-        const lastFeed = lastPage[lastPage.length - 1];
-        return lastFeed ? lastFeed.id : undefined;
-      },
-      initialPageParam: undefined, // Add this line
-    });
+// const FeedList = () => {
+//   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+//     useInfiniteQuery<Feed[], string[], string | undefined>({
+//       queryKey: ['feedList'],
+//       queryFn: async ({
+//         pageParam = 10,
+//       }: {
+//         pageParam?: number | undefined;
+//       }) => {
+//         const response = await getFeedList(pageParam);
+//         return response;
+//       },
+//       getNextPageParam: (lastPage) => {
+//         const lastFeed = lastPage[lastPage.length - 1];
+//         return lastFeed ? lastFeed.id : undefined;
+//       },
+//       initialPageParam: undefined, // Add this line
+//     });
 
-  const [ref, inView] = useInView({
-    threshold: 0,
-    rootMargin: '200px',
-  });
+//   const [ref, inView] = useInView({
+//     threshold: 0,
+//     rootMargin: '200px',
+//   });
 
-  React.useEffect(() => {
-    if (inView && hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+//   React.useEffect(() => {
+//     if (inView && hasNextPage && !isFetchingNextPage) {
+//       fetchNextPage();
+//     }
+//   }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
 
-  // if (!data?.pages) return <div>Loading...</div>;
-  return (
-    <div className="max-h-full w-10/12">
-      {data?.pages.map((page, pageIndex) => (
-        <React.Fragment key={pageIndex}>
-          {page.map((feed: Feed) => (
-            <FeedCard key={feed.id} feed={feed} />
-          ))}
-        </React.Fragment>
-      ))}
-      {isFetchingNextPage && <div>Loading...</div>}
-      <div ref={ref} />
-    </div>
-  );
-};
+//   // if (!data?.pages) return <div>Loading...</div>;
+//   return (
+//     <div className="max-h-full w-10/12">
+//       {data?.pages.map((page, pageIndex) => (
+//         <React.Fragment key={pageIndex}>
+//           {page.map((feed: Feed) => (
+//             <FeedCard key={feed.id} feed={feed} />
+//           ))}
+//         </React.Fragment>
+//       ))}
+//       {isFetchingNextPage && <div>Loading...</div>}
+//       <div ref={ref} />
+//     </div>
+//   );
+// };
 
-export default FeedList;
+// export default FeedList;
