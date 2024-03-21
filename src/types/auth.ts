5c9f59a72a36b5c375faf5ec0aa4208d29fb6720
devchaen <cogus1221@gmail.com>
2024-03-21 14:25:34 +0900

    [feat] Routes 코드 리팩토링 - createBrowserRouter  방식

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,3 +2,10 @@ export interface LoginBody {
   username: string;
   password: string;
 }
+
+export interface IUserInfoResponse {
+  id: string;
+  username: string;
+  email: string;
+  newUser: boolean;
+}
