67721aed350859036103994437ad0c49538f3c38
devchaen <cogus1221@gmail.com>
2024-03-28 13:00:02 +0900

    [test] 코드 수정

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,21 +1,24 @@
 import { Navigate, Outlet, useLoaderData } from 'react-router-dom';
 import useAuthStore from '../store/useAuthStore';
+import { useEffect } from 'react';
 import { IUserInfoResponse } from '@/types/auth';
 
 const PublicRoute = () => {
   const userData = useLoaderData() as IUserInfoResponse;
   const { setUser } = useAuthStore();
 
-  if (userData) {
-    setUser({
-      user: {
-        userId: userData.id,
-        userName: userData.username,
-        email: userData.email,
-        isNewUser: userData.newUser,
-      },
-    });
-  }
+  useEffect(() => {
+    if (userData) {
+      setUser({
+        user: {
+          userId: userData.id,
+          userName: userData.username,
+          email: userData.email,
+          isNewUser: userData.newUser,
+        },
+      });
+    }
+  }, []);
 
   return userData ? <Outlet /> : <Navigate to="/hello" />;
 };
