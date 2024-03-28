06a3e60dd146600f42d4955d4008ccbd1b0469de
devchaen <cogus1221@gmail.com>
2024-03-28 12:50:44 +0900

    [refactor] 불필요한 useEffect 제거

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,24 +1,21 @@
 import { Navigate, Outlet, useLoaderData } from 'react-router-dom';
 import useAuthStore from '../store/useAuthStore';
-import { useEffect } from 'react';
 import { IUserInfoResponse } from '@/types/auth';
 
 const PublicRoute = () => {
   const userData = useLoaderData() as IUserInfoResponse;
   const { setUser } = useAuthStore();
 
-  useEffect(() => {
-    if (userData) {
-      setUser({
-        user: {
-          userId: userData.id,
-          userName: userData.username,
-          email: userData.email,
-          isNewUser: userData.newUser,
-        },
-      });
-    }
-  }, []);
+  if (userData) {
+    setUser({
+      user: {
+        userId: userData.id,
+        userName: userData.username,
+        email: userData.email,
+        isNewUser: userData.newUser,
+      },
+    });
+  }
 
   return userData ? <Outlet /> : <Navigate to="/hello" />;
 };
