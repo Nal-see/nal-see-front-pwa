8684caca18839b997d48baf87eb0d40e138ab9bc
devchaen <cogus1221@gmail.com>
2024-04-07 18:51:37 +0900

    [feat] '이 지역 재검색' 버튼 컴포넌트 추가

---
+++ src/features/Home/components/RetrySearchOnMapBtn.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { RedoOutline } from 'antd-mobile-icons';
+import useHomeStore from '../store/useHomeStore';
+
+const RetrySearchOnMapBtn = () => {
+  const { isMapMoved } = useHomeStore();
+
+  const refetchOnMapMove = () => {};
+
+  return (
+    <>
+      {isMapMoved && (
+        <button
+          onClick={refetchOnMapMove}
+          className="absolute inset-x-0 bottom-6 z-[3] mx-auto inline-flex w-[130px] items-center justify-center gap-1 rounded-full border border-accent-foreground bg-white p-2 font-medium text-accent shadow-md"
+        >
+          <RedoOutline fontSize={16} />
+          <p>이 지역 재검색</p>
+        </button>
+      )}
+    </>
+  );
+};
+
+export default RetrySearchOnMapBtn;
--- src/features/Home/store/useHomeStore.ts
+++ src/features/Home/store/useHomeStore.ts
@@ -10,16 +10,20 @@ interface IPostGroupLocation {
 interface IHomeStore {
   selectedGroup: IPostGroupLocation | null;
   isPostDrawerOpen: boolean;
+  isMapMoved: boolean;
   setPostGroup: (selectedGroup: IPostGroupLocation) => void;
   setPostDrawerOpen: () => void;
+  setIsMapMoved: (value: boolean) => void;
 }
 
 const useHomeStore = create<IHomeStore>((set) => ({
   selectedGroup: null,
   isPostDrawerOpen: false,
+  isMapMoved: false,
   setPostGroup: (selectedGroup) => set({ selectedGroup: selectedGroup }),
   setPostDrawerOpen: () =>
     set((state) => ({ isPostDrawerOpen: !state.isPostDrawerOpen })),
+  setIsMapMoved: (value) => set({ isMapMoved: value }),
 }));
 
 export default useHomeStore;
