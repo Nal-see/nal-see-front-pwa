ca8fbbc27aada8802defe6711e3c36edf11db118
oyeong011 <oyeong011@gmail.com>
2024-04-17 17:21:58 +0900

    [type] 타입 에러 수정

--- src/services/fcm/firebase-config.ts
+++ src/services/fcm/firebase-config.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 import { getMessaging, onMessage } from 'firebase/messaging';
 import { useNotificationStore } from '@/store/useNotificationStore';
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const messaging = getMessaging(app);
 
 // foreground message 처리
