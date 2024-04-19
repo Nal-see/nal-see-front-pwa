e322ef281940b9848027fe50784a0f406f89f6fc
oyeong011 <oyeong011@gmail.com>
2024-04-19 15:03:38 +0900

    [fix] NS-46 채팅 나가기 api 수정 완료

--- src/features/Chat/services/chatApi.ts
+++ src/features/Chat/services/chatApi.ts
@@ -21,21 +21,10 @@ export const getChatMesg = async (chatId: string) => {
   return response.data;
 };
 
-//hihello
-
-export const hihello = async () => {
-  const response = await axios.get(
-    `${import.meta.env.VITE_API_BASE_URL}:8090/hihello`,
-    {
-      withCredentials: true,
-    },
-  );
-  return response.data;
-};
-
 export const exitChat = async (chatId: string) => {
   const response = await axios.post(
     `${import.meta.env.VITE_API_BASE_URL}:8090/exit-room?chatId=${chatId}`,
+    null,
     {
       withCredentials: true,
     },
