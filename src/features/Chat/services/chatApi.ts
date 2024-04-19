2be6ce9fe267e67d3ee417864ba2a0e2a5350df6
oyeong011 <oyeong011@gmail.com>
2024-04-19 14:04:01 +0900

    [fix] NS-46 채팅 나가기 재설정

--- src/features/Chat/services/chatApi.ts
+++ src/features/Chat/services/chatApi.ts
@@ -34,13 +34,6 @@ export const hihello = async () => {
 };
 
 export const exitChat = async (chatId: string) => {
-  const response1 = await axios.get(
-    `${import.meta.env.VITE_API_BASE_URL}:8090/hihello`,
-    {
-      withCredentials: true,
-    },
-  );
-  console.log(response1.data);
   const response = await axios.post(
     `${import.meta.env.VITE_API_BASE_URL}:8090/exit-room?chatId=${chatId}`,
     {
