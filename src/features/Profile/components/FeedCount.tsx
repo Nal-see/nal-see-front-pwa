65994843311f9fa2c706ceb1f5624e9efce26677
oyeong011 <oyeong011@gmail.com>
2024-04-19 16:10:36 +0900

    [feat] NS-12 팔로워 실시간 반영 기능 부여

--- src/features/Profile/components/FeedCount.tsx
+++ src/features/Profile/components/FeedCount.tsx
@@ -8,10 +8,16 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import CircleProfileImg from '@/components/CircleProfileImg';
-import { getFollowerList, getFollowingList } from '../services/profileApi';
+import {
+  followUser,
+  getFollowerList,
+  getFollowingList,
+  unFollowUser,
+} from '../services/profileApi';
 import { Button } from '@/components/ui/button';
-import useAuthStore from '@/store/useAuthStore';
 import { useNavigate } from 'react-router-dom';
+import { convertImgSrcToHTTPS } from '@/lib/helpers';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 type FeedCountProp = {
   count: string | number;
@@ -36,7 +42,6 @@ const FeedCount: React.FC<FeedCountProp> = ({
   const [isOpen, setIsOpen] = useState(false);
   const [users, setUsers] = useState<User[]>([]);
   const navigate = useNavigate();
-  const myId = useAuthStore.getState().user?.userId;
 
   const getFollowList = useCallback(
     async (type: string) => {
@@ -62,16 +67,48 @@ const FeedCount: React.FC<FeedCountProp> = ({
     navigate(`/user/${userId}`);
   };
 
-  // const { mutate: updateProfileMutation } = useMutation({
-  //   mutationFn: () => updateProfile(ProfileEditInfo),
-  //   onSuccess: () => {
-  //     onUpdateSuccess();
-  //     onClose();
-  //   },
-  //   onError: (error) => {
-  //     console.error('Failed to update profile:', error);
-  //   },
-  // });
+  const queryClient = useQueryClient();
+
+  const FollowMutation = useMutation({
+    mutationFn: (userId: string) => followUser(userId),
+    onSuccess: (_, userId) => {
+      console.log('followed success');
+      setUsers((prevUsers) =>
+        prevUsers.map((user) =>
+          user.userId === userId ? { ...user, isFollowed: true } : user,
+        ),
+      );
+      queryClient.invalidateQueries(['userProfileData', userId] as any);
+    },
+    onError: () => {
+      console.log('followed failed');
+    },
+  });
+
+  const UnFollowMutation = useMutation({
+    mutationFn: (userId: string) => unFollowUser(userId),
+    onSuccess: (_, userId) => {
+      console.log('unfollowed success');
+      setUsers((prevUsers) =>
+        prevUsers.map((user) =>
+          user.userId === userId ? { ...user, isFollowed: false } : user,
+        ),
+      );
+      queryClient.invalidateQueries(['userProfileData', userId] as any);
+    },
+    onError: () => {
+      console.log('unfollowed failed');
+    },
+  });
+
+  const handleFollow = (userId: string) => {
+    const isFollowed = users.find((user) => user.userId === userId)?.isFollowed;
+    if (isFollowed) {
+      UnFollowMutation.mutate(userId);
+    } else {
+      FollowMutation.mutate(userId);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center">
@@ -112,24 +149,25 @@ const FeedCount: React.FC<FeedCountProp> = ({
                       onClick={() => moveProfile(user.userId)}
                     >
                       <CircleProfileImg
-                        profileImgUrl={user.picture}
+                        profileImgUrl={convertImgSrcToHTTPS(user.picture)}
                         size="size-10"
                       />
                       <p className="text-base font-medium leading-none">
                         {user.username}
                       </p>
                     </div>
-                    {userId == myId ? (
-                      <Button
-                        className={`${
-                          !user.isFollowed
-                            ? 'bg-accent text-slate-50'
-                            : 'bg-secondary-foreground text-black'
-                        }`}
-                      >
-                        {`${!user.isFollowed ? '팔로우' : '팔로잉'}`}
-                      </Button>
-                    ) : null}
+                    {/* {userId == myId ? ( */}
+                    <Button
+                      onClick={() => handleFollow(user.userId)}
+                      className={`${
+                        !user.isFollowed
+                          ? 'bg-accent text-slate-50'
+                          : 'bg-secondary-foreground text-black'
+                      }`}
+                    >
+                      {`${!user.isFollowed ? '팔로우' : '팔로잉'}`}
+                    </Button>
+                    {/* ) : null} */}
                   </div>
                 ))}
               </div>
